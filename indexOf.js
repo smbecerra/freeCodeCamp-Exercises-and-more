ef656bf6e77a95be5633be59963682de784ea414
smbecerra <34818365+smbecerra@users.noreply.github.com>
2019-09-20 11:51:22 -0700

    Create indexOf.js

---
+++ indexOf.js
@@ -0,0 +1,11 @@
+function quickCheck(arr, elem) {
+ 
+if (arr.indexOf(elem) >= 0) {
+  return true
+} else {
+  return false
+}
+
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots', 'donkeys'], 'mushrooms'));
